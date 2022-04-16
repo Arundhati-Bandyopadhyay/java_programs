1211d4ef4d581d4458d092912fcd89e1684ed0df
Arundhati-Bandyopadhyay <77777188+Arundhati-Bandyopadhyay@users.noreply.github.com>
2022-04-17 00:08:38 +0530

    my java  files

--- oops/Constructor.java
+++ oops/Constructor.java
@@ -1,37 +1,52 @@
-class MyEmployee{
-     private int id;
-     private String name;
+class MyEmployee {
+    private int id;
+    private String name;
+    private int salary;
 
-    //  public MyEmployee(){
-    //      id=5;
-    //      name="Arundhatib";
-    //  }
-     public MyEmployee(String myName, int myid){
-        id=myid;
-        name=myName;
+    // public MyEmployee(){
+    // id=5;
+    // name="Arundhatib";
+    // }
+    public MyEmployee(String myName, int myid) {
+        id = myid;
+        name = myName;
     }
-     public String getName(){
-         return name;
-     }
-     public void setName(String n){
-         this.name=n;
-     }
-     public int getId(){
-         return id;
-     }
-     public void setId(int i){
-         this.id=i;
-     }
+
+    public MyEmployee(int empsalary) {
+        salary = empsalary;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n) {
+        this.name = n;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int i) {
+        this.id = i;
+    }
 }
+
 public class Constructor {
     public static void main(String[] args) {
-        //MyEmployee tinni= new MyEmployee();
-       MyEmployee tinni=new MyEmployee("arundhti", 5);
-       
-        System.out.println(tinni.getName());
-   
-       System.out.println(tinni.getId());
+        // MyEmployee tinni= new MyEmployee();
+        MyEmployee tinni = new MyEmployee("arundhti", 5);
+        MyEmployee s = new MyEmployee(10000);
 
+        System.out.println(tinni.getName());
+        System.out.println(tinni.getId());
+
+        System.out.println(s.getSalary());
 
     }
-} 
+}
