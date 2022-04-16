4632ce7e62b931984928b905e04c49431da03dd0
Arundhati-Bandyopadhyay <77777188+Arundhati-Bandyopadhyay@users.noreply.github.com>
2022-04-16 23:00:23 +0530

    my java programs

--- oops/Constructor.java
+++ oops/Constructor.java
@@ -2,10 +2,14 @@ class MyEmployee{
      private int id;
      private String name;
 
-     public MyEmployee(){
-         id=5;
-         name="Arundhatib";
-     }
+    //  public MyEmployee(){
+    //      id=5;
+    //      name="Arundhatib";
+    //  }
+     public MyEmployee(String myName, int myid){
+        id=myid;
+        name=myName;
+    }
      public String getName(){
          return name;
      }
@@ -21,9 +25,11 @@ class MyEmployee{
 }
 public class Constructor {
     public static void main(String[] args) {
-        MyEmployee tinni= new MyEmployee();
+        //MyEmployee tinni= new MyEmployee();
+       MyEmployee tinni=new MyEmployee("arundhti", 5);
        
         System.out.println(tinni.getName());
+        System.out.println(tinni.getId());
 
     }
 }
